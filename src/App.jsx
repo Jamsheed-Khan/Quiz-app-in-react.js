3d3c5bd8e52c470612b8af1af1af03a01a7a8892
muhammad-umar-218980 <muhammad.umar.218980@gmail.com>
2024-01-19 18:07:17 +0500

    working on it..

--- src/App.jsx
+++ src/App.jsx
@@ -3,95 +3,57 @@ import './App.css';
 
 function App() {
 
-  let [questions, setQuestion] = useState([]);
-  let [currentIndex, setcurrentIndex] = useState(0);
+  // let [cityname, setCityname] = useState("");
+  let [currentWeather, setcurrentWeather] = useState(0);
+  let [inputText, setinputText] = useState('')
+  let [data, setData] = useState([]);
+  let [value, setvalue] = useState("");
 
-
-  useEffect(function () {
-    getDataFromApi();
-  }, []);
-
-
-  function getDataFromApi() {
-    fetch("https://the-trivia-api.com/v2/questions")
+  function getDataFromApi(cityname) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=23452a215e99c24e452175b6ff4e8081&units=metric`)
       .then((data) => data.json())
-      .then((data) => {
-        data.map(function (item) {
-          item.options = [...item.incorrectAnswers, item.correctAnswer]
-          item.options = shuffle(item.options)
-        })
-        setQuestion(data)
-      })
+      .then((data) => setData(data));
   }
 
+  
+  // if (!data) {
+  //   return (
+  //     <h1>
+  //       <img src="https://media0.giphy.com/media/uIJBFZoOaifHf52MER/200w.gif?cid=6c09b952tx2bnjwjvxugevqfbzfpw4ms0l025y24ub9mtcvx&ep=v1_gifs_search&rid=200w.gif&ct=g"
+  //         alt="" />
+  //     </h1>
+  //   )
+  // }
+  console.log(data.main.temp);
 
 
-  function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
-
-    // While there remain elements to shuffle.
-    while (currentIndex > 0) {
-
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
+  const searchWeather = () => {
+    getDataFromApi(value);
+    setvalue("");
   }
 
-  // Used like so
-  var arr = [2, 11, 37, 42];
-  shuffle(arr);
-  console.log(arr);
-
-
-
-
-
-
-
-
-  if (!questions.length) {
-    return (
-      <h1>
-        <img src="https://media0.giphy.com/media/uIJBFZoOaifHf52MER/200w.gif?cid=6c09b952tx2bnjwjvxugevqfbzfpw4ms0l025y24ub9mtcvx&ep=v1_gifs_search&rid=200w.gif&ct=g"
-          alt="" />
-      </h1>
-    )
-  }
-
-  function restart() {
-    setcurrentIndex(0)
-  }
-  function nextQuestion() {
-    setcurrentIndex(currentIndex + 1)
-
-  }
   return (
     <div className="App-header">
-      <h4>{currentIndex + 1}) questions[currentIndex].question.text</h4>
-      {
-        questions[currentIndex].options.map(function (data) {
-          return <div>
-            <input type="radio" value={data} name='ques' id='' />{ data}
-          </div>
-        })
-      }
-      {
-        currentIndex === questions.length - 1 ? (
-          <button onClick={restart}>Restart</button>
-        ) :
-          (<button onClick={nextQuestion}>Next</button>
-          )
-    }
+      <div className="nameDiv">
+        <input type="text" value={value}  onChange={(e) => setvalue(e.target.value)}/>
+        <button onClick={searchWeather}>Search</button>
+        <p>{data.name}</p>
+          <p>
+            {data.main.temp}
+          </p>
+      </div>
     </div>
   )
+
 }
+
+
+
+
+
+
+
+
 export default App;
 
 
